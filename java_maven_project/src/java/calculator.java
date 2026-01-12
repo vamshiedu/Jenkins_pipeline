8229c1b99f87002c6f480f989e32922a0492bc67
vamshibituku <vamshibituku.edu@gmail.com>
2026-01-12 14:22:38 +0530

    Added maven

---
+++ java_maven_project/src/java/calculator.java
@@ -0,0 +1,10 @@
+package java;
+public class calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Maven build successful!");
+    }
+}
